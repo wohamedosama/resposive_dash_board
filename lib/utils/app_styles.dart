d02da5a21c57e935da70aafa13e58c255c62f611
mohamed osama <mohamedmedo9932@gmail.com>
2025-02-18 14:21:40 +0200

    add images to my project

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+abstract class AppStyles {
+  static const TextStyle styleRegular16 = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+    color: Color(0XFF064061),
+  );
+  static const TextStyle styleBold16 = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w700,
+    color: Color(0XFF4EB7F2),
+  );
+  static const TextStyle styleMedium16 = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w500,
+    color: Color(0XFF064061),
+  );
+  static const TextStyle styleMedium20 = TextStyle(
+    fontSize: 20,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w500,
+    color: Color(0XFFFFFFFF),
+  );
+  static const TextStyle styleSemiBold16 = TextStyle(
+    fontSize: 16,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+    color: Color(0XFF064061),
+  );
+  static const TextStyle styleSemiBold20 = TextStyle(
+    fontSize: 20,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+    color: Color(0XFF064061),
+  );
+  static const TextStyle styleRegular12 = TextStyle(
+    fontSize: 12,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+    color: Color(0XFFAAAAAA),
+  );
+  static const TextStyle styleSemiBold24 = TextStyle(
+    fontSize: 24,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+    color: Color(0XFF4EB7F2),
+  );
+  static const TextStyle styleRegular14 = TextStyle(
+    fontSize: 14,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w400,
+    color: Color(0XFFAAAAAA),
+  );
+  static const TextStyle styleSemiBold18 = TextStyle(
+    fontSize: 18,
+    fontFamily: 'Montserrat',
+    fontWeight: FontWeight.w600,
+    color: Color(0XFFFFFFFF),
+  );
+}
