683d79651eb40ec78ebb68a06e66ee6e9e4350c8
mohamed osama <mohamedmedo9932@gmail.com>
2025-02-19 13:47:40 +0200

    genrate assets and add font to the application

---
+++ lib/widgets/user_info_list_tile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UserInfoListTile extends StatelessWidget {
+  const UserInfoListTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
