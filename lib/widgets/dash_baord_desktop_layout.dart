11d9e0c2b27c18008a55a44f483c7979ecae1829
mohamed osama <mohamedmedo9932@gmail.com>
2025-02-19 14:33:26 +0200

    add font regulaer from type Montserrat and fix this issue

--- lib/widgets/dash_baord_desktop_layout.dart
+++ lib/widgets/dash_baord_desktop_layout.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:resposive_dash_board/widgets/custom_drawer.dart';
 
 class DashBaordDesktopLayout extends StatelessWidget {
   const DashBaordDesktopLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Row(
+      children: [
+        Expanded(
+          child: CustomDrawer(),
+        ),
+      ],
+    );
   }
 }
--- lib/widgets/user_info_list_tile.dart
+++ lib/widgets/user_info_list_tile.dart
@@ -13,15 +13,19 @@ class UserInfoListTile extends StatelessWidget {
   final String image, title, subTitle;
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: SvgPicture.asset(image),
-      title: Text(
-        title,
-        style: AppStyles.styleSemiBold16,
-      ),
-      subtitle: Text(
-        subTitle,
-        style: AppStyles.styleRegular12,
+    return Card(
+      elevation: 0,
+      color: Color(0XFFFAFAFA),
+      child: ListTile(
+        leading: SvgPicture.asset(image),
+        title: Text(
+          title,
+          style: AppStyles.styleSemiBold16,
+        ),
+        subtitle: Text(
+          subTitle,
+          style: AppStyles.styleRegular12,
+        ),
       ),
     );
   }
