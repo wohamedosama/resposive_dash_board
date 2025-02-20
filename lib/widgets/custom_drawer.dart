87a5ffc314120974718f990031792bf363949694
mohamed osama <mohamedmedo9932@gmail.com>
2025-02-20 13:30:38 +0200

    create drawer item model to handle the drawer item and create list view of type drawer item model

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:resposive_dash_board/model/drawer_item_model.dart';
 import 'package:resposive_dash_board/utils/app_images.dart';
+import 'package:resposive_dash_board/widgets/drawer_item.dart';
 import 'package:resposive_dash_board/widgets/user_info_list_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
+  static const List<DrawerItemModel> drawerItemModelList = [
+    DrawerItemModel(title: 'Dashboard', image: Assets.imagesDashboard),
+    DrawerItemModel(title: 'Messages', image: Assets.imagesMyTrasnactions),
+    DrawerItemModel(title: 'Statistics', image: Assets.imagesStatistics),
+    DrawerItemModel(title: 'Wallet Account', image: Assets.imagesWallet2),
+    DrawerItemModel(title: 'My Invetments', image: Assets.imagesMyInvetments),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +24,18 @@ class CustomDrawer extends StatelessWidget {
             image: Assets.imagesAvatar3,
             title: 'Lekan Okeowo',
             subTitle: 'demo@gmail.com',
+          ),
+          SizedBox(height: 8),
+          ListView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: DrawerItem(drawerItemModel: drawerItemModelList[index]),
+              );
+            },
+            itemCount: drawerItemModelList.length,
           ),
         ],
       ),
---
+++ lib/widgets/drawer_item.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:resposive_dash_board/model/drawer_item_model.dart';
+import 'package:resposive_dash_board/utils/app_styles.dart';
+
+class DrawerItem extends StatelessWidget {
+  const DrawerItem({super.key, required this.drawerItemModel});
+  final DrawerItemModel drawerItemModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(drawerItemModel.image),
+      title: Text(drawerItemModel.title, style: AppStyles.styleRegular16),
+    );
+  }
+}
